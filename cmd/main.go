b49227b2ba932670091a2839dd9445a32f910de7
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2022-07-29 13:55:31 +0300

    Upload cmd file

---
+++ cmd/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	handlers "github.com/O-Mercan/Product--Service-V2/handlers"
+	models "github.com/O-Mercan/Product--Service-V2/models"
+	repo "github.com/O-Mercan/Product--Service-V2/repositories"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+
+	log.Println("Server starting...")
+
+	product := models.Product{
+		Name:        "Samsung Z Fold",
+		Category:    "Tech",
+		Summary:     "asdasd",
+		Description: "asdsdas",
+		Price:       6000,
+	}
+	repo.InsertProduct(product)
+
+	//repo.GetProducts()
+	//repo.GetProductByID(2)
+
+	/* product := models.Product{
+		Id:          3,
+		Name:        "İphone 6",
+		Category:    "Tech",
+		Summary:     "asdasd",
+		Description: "asdsdas",
+		Price:       2000,
+	}
+	repo.UpdateProduct(product) */
+	//repo.DeleteProductByID(4)
+	//repo.GetProducts()
+
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/api/products", handlers.GetProductsHandler).Methods("GET")
+	r.HandleFunc("/api/products/{id}", handlers.GetProductHandler).Methods("GET")
+	r.HandleFunc("/api/products", handlers.PostProductHandler).Methods("POST")
+	r.HandleFunc("/api/products", handlers.PutProductHandler).Methods("PUT")
+	r.HandleFunc("/api/products", handlers.DeleteProductHandler).Methods("DELETE")
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	server.ListenAndServe()
+
+	log.Println("Server ending...")
+
+}
